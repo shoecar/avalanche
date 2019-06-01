52a3be7735e999251f78b4412e9b2b370d0c4f2c
Tad Schukar <tschukar@gmail.com>
2019-06-01 16:00:18 -0700

    game now runs with rounds

--- js/game.js
+++ js/game.js
@@ -1,106 +1,122 @@
 (function () {
-  if (typeof Ava === "undefined") {
-    window.Ava = {};
-  }
+    if (typeof Ava === 'undefined') {
+        window.Ava = {};
+    }
 
-  var Coord = Ava.Coord = function (i, j) {
-    this.i = i;
-    this.j = j;
-  };
+    var Coord = Ava.Coord = function (i, j) {
+        this.i = i;
+        this.j = j;
+    };
 
-  Coord.prototype.equals = function (coord2) {
-    return (this.i == coord2.i) && (this.j == coord2.j);
-  };
+    Coord.prototype.equals = function (coord2) {
+        return (this.i == coord2.i) && (this.j == coord2.j);
+    };
 
-  Coord.prototype.isOpposite = function (coord2) {
-    return (this.i == (-1 * coord2.i)) && (this.j == (-1 * coord2.j));
-  };
+    Coord.prototype.isOpposite = function (coord2) {
+        return (this.i == (-1 * coord2.i)) && (this.j == (-1 * coord2.j));
+    };
 
-  Coord.prototype.plus = function (coord2) {
-    var i = this.i + coord2.i;
-    var j = this.j + coord2.j;
+    Coord.prototype.plus = function (coord2) {
+        var i = this.i + coord2.i;
+        var j = this.j + coord2.j;
 
-    return new Coord(i, j);
-  };
+        return new Coord(i, j);
+    };
 
-  var Player = Ava.Player = function (board, startX) {
-    this.faceLeft = true;
-    this.alive = true;
-    this.board = board;
-    this.position = new Coord(board.gridHeight - 1, startX ? startX : Math.floor(board.gridWidth / 2));
-  };
+    var Player = Ava.Player = function (board, playerNumber) {
+        this.playerNumber = playerNumber;
+        this.faceLeft = true;
+        this.alive = true;
+        this.board = board;
+        this.cycles = 0;
+        this.position = new Coord(board.gridHeight - 1, playerNumber ? playerNumber : Math.floor(board.gridWidth / 2));
+    };
 
-  Player.DIFFS = {
-    "L": new Coord(0, -1),
-    "R": new Coord(0, 1)
-  };
+    Player.DIFFS = {
+        L: new Coord(0, -1),
+        R: new Coord(0, 1)
+    };
 
-  Player.prototype.move = function (dir) {
-    this.faceLeft = dir === "L";
-    var tempPos = this.position.plus(Player.DIFFS[dir]);
-    var i = this.board.loopPos(tempPos.i, this.board.gridHeight);
-    var j = this.board.loopPos(tempPos.j, this.board.gridWidth);
-    this.position = new Coord(i, j);
-  };
+    Player.prototype.move = function () {
+        if (Math.random() < 0.95) {
+            return;
+        }
+        var direction = Math.random() < .5 ? 'L' : 'R';
+        this.faceLeft = direction === 'L';
+        var tempPos = this.position.plus(Player.DIFFS[direction]);
+        var i = this.board.loopPos(tempPos.i, this.board.gridHeight);
+        var j = this.board.loopPos(tempPos.j, this.board.gridWidth);
+        this.position = new Coord(i, j);
+    };
 
-  var Ice = Ava.Ice = function (position, waveTime) {
-    this.position = position;
-    this.waveTime = waveTime;
-    this.dropPoint = 0;
-  };
+    var Ice = Ava.Ice = function (position, waveTime) {
+        this.position = position;
+        this.waveTime = waveTime;
+        this.dropPoint = 0;
+    };
 
-  var Board = Ava.Board = function (gridHeight, gridWidth, waveTime, speed) {
-    this.gridHeight = gridHeight;
-    this.gridWidth = gridWidth;
-    this.waveTime = waveTime;
-    this.speed = speed;
-    this.iteration = 1;
-    this.player = new Player(this);
-    this.players = [];
-    this.icicles = [];
+    var Board = Ava.Board = function (gridHeight, gridWidth, waveTime, speed) {
+        this.gridHeight = gridHeight;
+        this.gridWidth = gridWidth;
+        this.waveTime = waveTime;
+        this.speed = speed;
+        this.cycles = 0;
+        this.players = [];
+        this.playerResults = [];
+        this.icicles = [];
 
-    this.addPlayers(PLAYERS_COUNT);
-    window.setTimeout(function () {
-      this.startWave();
-    }.bind(this), 500)
-  };
+        this.addPlayers(PLAYERS_COUNT);
+        window.setTimeout(function () {
+            this.startWave();
+        }.bind(this), 500)
+    };
 
-  Board.prototype.cycle = function () {
-    this.icicles.forEach(function (ice) {
-      ice.dropPoint += ice.waveTime;
-      if (ice.dropPoint > this.speed) {
-        ice.dropPoint = 0;
-        ice.position.i++;
-        if (ice.position.equals(this.player.position)) {
-          this.player.alive = false;
-        };
+    Board.prototype.cycle = function () {
+        this.cycles++;
+
+        var deadlyIce = [];
+        this.icicles = this.icicles.filter(function (ice) {
+            ice.dropPoint += ice.waveTime;
+            if (ice.dropPoint > this.speed) {
+                ice.dropPoint = 0;
+                ice.position.i++;
+            }
+            if (ice.position.i === this.gridHeight - 1) {
+                deadlyIce.push(ice.position.j);
+            }
+            return ice.position.i < this.gridHeight;
+        }.bind(this));
+
         this.players = this.players.filter(function (player) {
-          return !(ice.position.equals(player.position));
-        });
-      }
-    }.bind(this));
+            if (deadlyIce.indexOf(player.position.j) > -1) {
+                player.cycles = this.cycles;
+                this.playerResults.push(player);
+                return false;
+            }
+            player.move();
+            return true;
+        }.bind(this));
 
-    this.iteration++;
-    if (this.iteration % this.waveTime === 0) {
-      this.startWave();
-    }
-  };
+        if (this.cycles % this.waveTime === 0) {
+            this.startWave();
+        }
+    };
 
-  Board.prototype.addPlayers = function (playersCount) {
-    for (var i = 0; i < playersCount; i++) {
-      this.players.push(new Ava.Player(this, i));
-    }
-  };
+    Board.prototype.addPlayers = function (playersCount) {
+        for (var i = 0; i < playersCount; i++) {
+            this.players.push(new Ava.Player(this, i));
+        }
+    };
 
-  Board.prototype.startWave = function () {
-    for (var i = 0; i < this.gridWidth; i++) {
-      this.icicles.push(new Ava.Ice(new Coord(0, i), Math.ceil(Math.random() * 100) + 50));
-    }
-  };
+    Board.prototype.startWave = function () {
+        for (var i = 0; i < this.gridWidth; i++) {
+            this.icicles.push(new Ava.Ice(new Coord(0, i), Math.ceil(Math.random() * 100) + 50));
+        }
+    };
 
-  Board.prototype.loopPos = function (position, max) {
-    if (position < 0) { return max - 1; }
-    if (position >= max) { return 0; }
-    return position;
-  };
+    Board.prototype.loopPos = function (position, max) {
+        if (position < 0) { return max - 1; }
+        if (position >= max) { return 0; }
+        return position;
+    };
 })();
--- js/view.js
+++ js/view.js
@@ -1,126 +1,148 @@
 (function () {
-  if (typeof Ava === "undefined") {
-    window.Ava = {};
-  }
+    window.Ava = window.Ava || {};
 
-  var View = Ava.View = function ($el, milliS) {
-    this.$el = $el;
-    this.milliS = milliS;
-    this.gridHeight = 20;
-    this.gridWidth = 20;
-    this.waveTime = 200;
-    this.speed = 700;
-    this.board = new Ava.Board(this.gridHeight, this.gridWidth, this.waveTime, this.speed);
-    this.buildGrid();
+    var View = Ava.View = function ($el, milliS) {
+        this.$el = $el;
+        this.milliS = milliS;
+        this.gridHeight = VIEW_HEIGHT;
+        this.gridWidth = VIEW_WIDTH;
+        this.waveTime = ICE_WAVE_TIME;
+        this.speed = ICE_SPEED;
+        this.round = 0;
+        this.buildGrid();
 
-    $(window).bind('keydown', this.handleKeyEvent.bind(this));
+        $(window).bind('keydown', this.handleKeyEvent.bind(this));
+        $('#start-round').click(this.startRound.bind(this));
 
-    this.intervalId = window.setInterval(this.step.bind(this), this.milliS);
+        this.scoreCookie = new Ava.Cookie();
 
-    this.scoreCookie = new Ava.Cookie();
-  }
+        this.startRound();
+    }
 
-  View.KEYS = {
-    37: 'L',
-    65: 'L',
-    39: 'R',
-    68: 'R'
-  };
+    View.KEYS = {
+        37: 'L',
+        65: 'L',
+        39: 'R',
+        68: 'R'
+    };
 
-  View.prototype.handleKeyEvent = function (event) {
-    if (!this.intervalId) {
-      this.board.player.alive = true;
-      this.$el.find('.paused').remove();
-      this.render();
-      this.intervalId = window.setInterval(this.step.bind(this), this.milliS);
-    } else {
-      if (View.KEYS[event.keyCode]) {
-        this.board.player.move(View.KEYS[event.keyCode]);
-        this.moveBG(View.KEYS[event.keyCode]);
-      } else if (event.keyCode === 32 && this.board.player.alive) {
+    View.prototype.handleKeyEvent = function (event) {
+        if (event.keyCode === 27) {
+            window.clearInterval(this.intervalId);
+            this.escape();
+        } else if (!this.intervalId) {
+            this.$el.find('.paused').remove();
+            this.paint();
+            this.intervalId = window.setInterval(this.step.bind(this), this.milliS);
+        } else {
+            if (View.KEYS[event.keyCode]) {
+                this.moveBG(View.KEYS[event.keyCode]);
+            } else if (event.keyCode === 32) {
+                window.clearInterval(this.intervalId);
+                this.intervalId = null;
+                this.updateClasses([], 'ice');
+                this.$el.prepend('<div class="paused">Game Paused</div>');
+            }
+        }
+
+    };
+
+    View.prototype.escape = function () {
         window.clearInterval(this.intervalId);
-        this.intervalId = null;
-        this.updateClasses([], 'ice');
-        this.$el.prepend('<div class="paused">Game Paused</div>');
-      }
-    }
+        $(window).unbind('keydown');
+    };
 
-  };
+    View.prototype.startRound = function () {
+        $('#round-info').text('Round ' + ++this.round);
+        this.board = new Ava.Board(this.gridHeight, this.gridWidth, this.waveTime, this.speed, this.milliS);
+        $('#round-over').hide();
 
-  View.prototype.render = function () {
-    var faceLeft = this.board.player.faceLeft ? 'left' : null;
-    this.updateClasses(this.board.players.concat(this.board.player), 'player', faceLeft);
-    this.updateClasses(this.board.icicles, 'ice');
-  };
+        try {
+            this.intervalId = window.setInterval(this.step.bind(this), this.milliS);
+        } catch (exception) {
+            window.clearInterval(this.intervalId);
+            console.log(exception);
+        }
+    };
 
-  View.prototype.updateClasses = function(collection, className, faceLeft) {
-    this.$cells.filter('.' + className).removeClass();
-    faceLeft && this.$cells.filter('.left').removeClass();
+    View.prototype.endRound = function () {
+        window.clearInterval(this.intervalId);
+        this.setHighScore(this.board.cycles);
+        $('#round-score').html(this.board.playerResults.map(function (player) {
+            return 'player: ' + player.playerNumber + ' cycles: ' + player.cycles;
+        }).join('<br>'));
+        $('#round-over').fadeIn();
+        // window.setTimeout(function () {
+        //     this.startRound();
+        // }.bind(this), ROUND_INTERMISSION_MS);
+    };
 
-    collection.forEach(function (model) {
-      var flatCoord = (model.position.i * this.gridWidth) + model.position.j;
-      this.$cells.eq(flatCoord).addClass(className);
-      faceLeft && this.$cells.eq(flatCoord).addClass('left');
-    }.bind(this));
-  };
+    View.prototype.step = function () {
+        $('#score-info').text(this.board.cycles);
+        if (this.board.players.length) {
+            this.board.cycle();
+            this.paint();
+        } else {
+            this.endRound();
+        }
+    };
 
-  View.prototype.buildGrid = function () {
-    this.$el.append('<div id="score" data-score="0.000">0.00s</div>')
-    for (var i = 0; i < this.gridHeight; i++) {
-      var row = $('<div class="row">');
-      for (var j = 0; j < this.gridWidth; j++) {
-        var cell = $('<pre class="cell">');
-        row.append(cell);
-      }
-      this.$el.append(row);
-    }
-    this.$cells = this.$el.find('pre');
-  };
+    View.prototype.paint = function () {
+        this.updateClasses(this.board.players, 'player');
+        this.updateClasses(this.board.icicles, 'ice');
+    };
 
-  View.prototype.step = function () {
-    var score = parseFloat($('#score').data('score')) + this.milliS / 1000;
-    $('#score').data('score', score.toFixed(3));
-    $('#score').text(score.toFixed(2) + 's');
-    if (this.board.player.alive) {
-      this.board.cycle();
-      this.render();
-    } else {
-      $('#score').text('');
-      $('#final-score').text('You survived for ' + score.toFixed(2) + ' seconds')
-      this.setHighScore(score);
-      $('#game-over').fadeIn();
-      $(window).unbind('keydown');
-      window.clearInterval(this.intervalId);
-    }
-  };
+    View.prototype.updateClasses = function(collection, className) {
+        this.$cells.filter('.' + className).removeClass();
 
-  View.prototype.moveBG = function (dir) {
-    if (dir === 'L') {
-      var pos1 = $('#avalanche').data('pos1') - 9;
-      var pos2 = $('#avalanche').data('pos2') - 20;
-      var pos3 = $('#avalanche').data('pos3') - 40;
-      $('#avalanche').css('background-position', '-' + pos3 + 'px 450px, -' + pos2 + 'px 270px, -' + pos1 + 'px 0px');
-      $('#avalanche').data('pos1', pos1);
-      $('#avalanche').data('pos2', pos2);
-      $('#avalanche').data('pos3', pos3);
-    } else {
-      var pos1 = $('#avalanche').data('pos1') + 9;
-      var pos2 = $('#avalanche').data('pos2') + 20;
-      var pos3 = $('#avalanche').data('pos3') + 40;
-      $('#avalanche').css('background-position', '-' + pos3 + 'px 450px, -' + pos2 + 'px 270px, -' + pos1 + 'px 0px')
-      $('#avalanche').data('pos1', pos1);
-      $('#avalanche').data('pos2', pos2);
-      $('#avalanche').data('pos3', pos3);
-    }
-  };
+        collection.forEach(function (model) {
+            var flatCoord = (model.position.i * this.gridWidth) + model.position.j;
+            this.$cells.eq(flatCoord).addClass(className);
+            model.faceLeft && this.$cells.eq(flatCoord).addClass('left');
+        }.bind(this));
+    };
 
-  View.prototype.setHighScore = function (score) {
-    var speed = 'medium';
-    if (this.milliS === 20) { speed = 'slow'; }
-    if (this.milliS === 5) { speed = 'fast'; }
-    if (score > this.scoreCookie.read(speed)) {
-      this.scoreCookie.create(speed, score.toFixed(2));
-      $('#high-score').text(score.toFixed(2) + ' seconds');
-    }
-  };
+    View.prototype.buildGrid = function () {
+        this.$el.append('<div id="round-info" class="grid-info" data-round="1">Round 1</div>')
+        this.$el.append('<div id="score-info" class="grid-info" data-score="0.000">0.00s</div>')
+        for (var i = 0; i < this.gridHeight; i++) {
+            var row = $('<div class="row">');
+            for (var j = 0; j < this.gridWidth; j++) {
+                var cell = $('<pre class="cell">');
+                row.append(cell);
+            }
+            this.$el.append(row);
+        }
+        this.$cells = this.$el.find('pre');
+    };
+
+    View.prototype.moveBG = function (dir) {
+        if (dir === 'L') {
+            var pos1 = $('#avalanche').data('pos1') - 9;
+            var pos2 = $('#avalanche').data('pos2') - 20;
+            var pos3 = $('#avalanche').data('pos3') - 40;
+            $('#avalanche').css('background-position', '-' + pos3 + 'px 450px, -' + pos2 + 'px 270px, -' + pos1 + 'px 0px');
+            $('#avalanche').data('pos1', pos1);
+            $('#avalanche').data('pos2', pos2);
+            $('#avalanche').data('pos3', pos3);
+        } else {
+            var pos1 = $('#avalanche').data('pos1') + 9;
+            var pos2 = $('#avalanche').data('pos2') + 20;
+            var pos3 = $('#avalanche').data('pos3') + 40;
+            $('#avalanche').css('background-position', '-' + pos3 + 'px 450px, -' + pos2 + 'px 270px, -' + pos1 + 'px 0px')
+            $('#avalanche').data('pos1', pos1);
+            $('#avalanche').data('pos2', pos2);
+            $('#avalanche').data('pos3', pos3);
+        }
+    };
+
+    View.prototype.setHighScore = function (score) {
+        var speed = 'fast';
+        if (this.milliS === 20) { speed = 'slow'; }
+        if (this.milliS === 10) { speed = 'medium'; }
+        if (score > this.scoreCookie.read(speed)) {
+            this.scoreCookie.create(speed, score);
+            $('#high-score').text(score);
+        }
+    };
 })();
